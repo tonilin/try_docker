ef7e77ae84c2edb1dcc24e1b1537ed2177d39393
tonilin <tonilin@gmail.com>
2015-04-17 16:12:49 +0800

    success deploy but not run

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:tonilin/try_docker.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, "/home/apps/try_docker"
 
 # Default value for :scm is :git
 # set :scm, :git
