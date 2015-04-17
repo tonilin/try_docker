d49bcfba921de7a2ff2787b61196139a14ad2ee8
tonilin <tonilin@gmail.com>
2015-04-17 15:29:32 +0800

    set repo_url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'try_docker'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:tonilin/try_docker.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
