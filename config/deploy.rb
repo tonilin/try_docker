99fd8189fd166be59881cd38f73afbe5a983e5b9
tonilin <tonilin@gmail.com>
2015-04-18 22:30:42 +0800

    refactor

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,10 +39,9 @@ set :release_name, "#{Time.now.utc.strftime("%Y%m%d%H%M%S")}/subdirectory"
 
 namespace :deploy do
 
-  after :published, "deploy_docker" do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      within "#{current_path}/docker" do
+  after :published, :update_mtime do
+    on roles(:web) do
+      within current_path do
 
         ["Gemfile", "Gemfile.lock", "docker/nginx-try-docker.conf", "docker/nginx.conf"].each do |file|
           command = "git rev-list HEAD \"#{file}\" | head -n 1"
@@ -51,9 +50,16 @@ namespace :deploy do
           command = "git show --pretty=format:%ai --abbrev-commit #{file_revision_hash} | head -n 1"
           file_modified_time = `#{command}`
           execute 'touch', "-d \"#{file_modified_time}\" #{current_path}/#{file}"
-
         end
 
+      end
+    end
+  end
+
+  after :update_mtime, :deploy_docker do
+    on roles(:web) do
+      # Here we can do anything such as:
+      within "#{current_path}/docker" do
         upload! "./docker/.env.web", "#{release_path}/docker/.env.web"
         execute 'docker-compose', 'build'
         execute 'docker', 'rmi $(docker images | grep "^<none>" | awk "{print $3}"); true'
